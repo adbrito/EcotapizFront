ded5fbc5c9ef28296e6d7edbf59a34026f0ff202
Iliana <iliesbol@espol.edu.ec>
2020-06-12 15:46:26 -0500

    paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './fijos/header/header.component';
+import { FooterComponent } from './fijos/footer/footer.component';
+import { HomeComponent } from './paginas/home/home.component';
+import { SimuladorComponent } from './paginas/simulador/simulador.component';
+import { CatalogoComponent } from './paginas/catalogo/catalogo.component';
+import { BlogComponent } from './paginas/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SimuladorComponent,
+    CatalogoComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
