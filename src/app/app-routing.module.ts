aed901ba3f38bc19da35bc145742f5da6d1a1f9a
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2020-06-24 20:20:10 -0500

    disque haciendo el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { BlogComponent } from './paginas/blog/blog.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'blog', component: BlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
