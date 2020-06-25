db694633968d7778579dc18fb75d9c18ff0b4792
Allison Brito <adbrito@espol.edu.ec>
2020-06-24 20:59:58 -0500

    resolviendo conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,17 @@ import { BlogComponent } from './paginas/blog/blog.component';
 
 import { AppComponent } from './app.component';
 
-<<<<<<< HEAD
-const routes: Routes = [{ path: 'app', loadChildren: () => import('./administrador/administrador.module').then(m => m.AdministradorModule) }];
-=======
+
 
 const routes: Routes =
   [{
     path: 'home',
     component: AppComponent,
   },
+  { path: 'app', loadChildren: () => import('./administrador/administrador.module').then(m => m.AdministradorModule) },
   { path: 'simulador', loadChildren: () => import('./paginas/simulador/simulador.module').then(m => m.SimuladorModule) },
   {path: '**', component: AppComponent}];
->>>>>>> 9e44ef173426044a1af35a1f0ffeb5c2072943f6
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
