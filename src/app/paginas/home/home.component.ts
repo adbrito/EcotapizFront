32c08082931d38bab9c68ca48922a9e111344c0a
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2020-07-23 23:58:45 -0500

    Funcional pagina principal

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -21,7 +21,6 @@ export class HomeComponent implements OnInit,AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.loadCatalogo(0);
   }
 
   startSteps(){
@@ -88,40 +87,4 @@ export class HomeComponent implements OnInit,AfterViewInit {
     }).start();
   }
 
-  loadCatalogo(contador: number){
-    var contenedor = this.elementRef.nativeElement.querySelector('#contenedor-catalogo');
-    $("div").remove(".example2");
-
-    let json: any = (data2 as any).default;
-    console.log(json)
-    let i: number;
-
-    for(i=5; i>-1; i--){
-
-      let j:number = contador*6 + i;
-
-      let titulo = json[j].titulo
-      let link = json[j].link
-      let contenido = json[j].contenido
-      let src = json[i].src
-
-      console.log(json[j].titulo);
-
-      let plantilla = `<div class="col-lg-4 col-sm-6 portfolio-item pb-4 example2">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src=${src} alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">${titulo}</a>
-          </h4>
-          <p class="card-text">${contenido}</p>
-        </div>
-      </div>
-    </div>`
-
-      contenedor.insertAdjacentHTML('afterbegin', plantilla);
-    }
-
-    
-  }
 }
