4c8d2d8edb50c2939bcef23da1e8907fba47788b
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2020-07-23 20:34:15 -0500

    Cambios Generales

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import * as introJs from 'intro.js/intro.js';
 import * as data from '../../../assets/recursos-datos/posts.json';
 import * as data2 from '../../../assets/recursos-datos/productos.json';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -29,57 +30,57 @@ export class HomeComponent implements OnInit,AfterViewInit {
       steps: [
         {
           element: '#step1',
-          intro: 'Welcome to your new app!',
+          intro: 'Bienvenidos a Ecotapiz, sere su guia por este sitio',
           position: 'bottom'
         },
         {
           element: '#step2',
-          intro: "Ok, wasn't that fun?",
+          intro: "En esta seccion, se presentara las noticias mas destacadas sobre el mundo del tapizado",
           position: 'right'
         },
         {
           element: '#step3',
-          intro: "let's keep going",
+          intro: "Aqui le mostraremos los productos mas solicitados por nuestros compradores",
           position: 'top'
         },
         {
           element: '#step4',
-          intro: 'More features, more fun.',
+          intro: 'Aqui se encuentran videos subidos por la empresa',
           position: 'right'
         },
         {
           element: '#step5',
-          intro: 'More features, more fun.',
+          intro: 'Al presionar esta opcion, se dirigirá a nuestra pagina de contacto, donde podra ver nuestra ubicacion y dejarnos sus mensajes',
           position: 'right'
         },
         {
           element: '#step6',
-          intro: 'More features, more fun.',
+          intro: 'Al seleccionar este menu desplegable, le aparecera tres opciones donde podra ir nuestra pagina principal, ir a la informacion de nuestra empresa e dirigirse a la seccion de contacto.',
           position: 'right'
         },
         {
           element: '#step7',
-          intro: 'More features, more fun.',
+          intro: 'Al presionar, se dirigira a la seccion de simulacion, donde podra modelar su tapizado de carro, segun su preferencia.',
           position: 'right'
         },
         {
           element: '#step8',
-          intro: 'More features, more fun.',
+          intro: 'En esta opcion, podra ver el listado de productos que ofrecemos',
           position: 'right'
         },
         {
           element: '#step9',
-          intro: 'More features, more fun.',
+          intro: 'Aqui encontrara las noticias sobre promociones, y eventos pasados de nuestra compañia, ademas sobre informacion del mundo del tapizado ',
           position: 'right'
         },
         {
           element: '#step10',
-          intro: 'More features, more fun.',
+          intro: 'Aqui al estar sobre el boton de buscar, le aparecera un recuadro que le permitira escribir lo que desea buscar en el sitio web',
           position: 'right'
         },
         {
           element: '#step11',
-          intro: 'More features, more fun.',
+          intro: 'Esta opcion solo es para los administradores del sitio, y solo podra iniciar sesion si cuenta con un usuario y contraseña',
           position: 'right'
         }
       ],
