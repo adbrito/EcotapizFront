e19d93795a0e21e814aa38243dd42eeb68268796
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2020-07-23 22:04:22 -0500

    arreglo

--- src/app/paginas/home/post-container/post-container.component.ts
+++ src/app/paginas/home/post-container/post-container.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { CatalogoDirective } from '../../main-catalogo/catalogo.directive';
+import * as posts from '../../../../assets/recursos-datos/posts.json';
+import { Catalogo2Directive } from '../../main-catalogo/catalogo2.directive';
+import { Catalogo3Directive } from '../../main-catalogo/catalogo3.directive';
+import { DynamicComponentService } from '../../main-catalogo/dynamic-component.service';
+import { DataTransferService } from '../../main-blog/data-transfer.service';
 @Component({
   selector: 'app-post-container',
   templateUrl: './post-container.component.html',
@@ -7,7 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostContainerComponent implements OnInit {
 
-  constructor() { }
+  contador: number;
+
+  @ViewChild(CatalogoDirective,{static: true}) post1: CatalogoDirective;
+  @ViewChild(Catalogo2Directive,{static: true}) post2: Catalogo2Directive;
+  @ViewChild(Catalogo3Directive,{static: true}) post3: Catalogo3Directive;
+
+  lista:ViewContainerRef[] = []
+
+  constructor(private service: DynamicComponentService, private data: DataTransferService) { }
 
   ngOnInit(): void {
   }
